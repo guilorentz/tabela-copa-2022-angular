0f2307e0e76bf4785553628db1419b0f33690603
guilorentz <go.lorentz@gmail.com>
2022-10-02 16:53:28 -0400

    add groups names

--- src/app/components/card-grupo/card-grupo.component.ts
+++ src/app/components/card-grupo/card-grupo.component.ts
@@ -7,19 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardGrupoComponent implements OnInit {
   @Input() grupo: any
-  @Input() index: number | undefined
-
-  nomeGrupo = {
-    0: "A",
-    1: "B",
-    2: "C",
-    3: "D",
-    4: "E",
-    5: "F",
-    6: "G",
-    7: "H"
-  }
-
 
   constructor() { }
 
