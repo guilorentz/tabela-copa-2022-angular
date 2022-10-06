d6015dfe66d26d59bf774c33a341dfb3b0402c20
guilorentz <go.lorentz@gmail.com>
2022-10-05 21:35:59 -0400

    add route for fase-grupos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { FaseGruposComponent } from './pages/fase-grupos/fase-grupos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'fase-grupos',
+    component: FaseGruposComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
